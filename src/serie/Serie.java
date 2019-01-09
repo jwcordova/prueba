3315a4aa82effef58687a333aa59390671313f79
jowa <jowa@dell>
2019-01-08 23:08:26 -0500

    codigo nuevo

--- src/serie/Serie.java
+++ src/serie/Serie.java
@@ -1,21 +1,31 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package serie;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
 
-/**
- *
- * @author jowa
- */
-public class Serie {
+public class serie {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws FileNotFoundException{
+        //PrintStream file = new PrintStream("Resultado.txt");
+        Scanner e = new Scanner(System.in);
+        System.out.print("Serie de Numeros Primos \n");
+        System.out.print("Ingresa el numero de elementos de la serie: ");
+               
+        int numIng = e.nextInt();
+        int c = 1;
+        int p = 2;
+        int d = 2;
+        
+        while (c <= numIng) {
+            if (p % d == 0) {
+                if (p == d) {
+                    System.out.print(p + ", ");
+                    c++;
+                }
+                d = 2;
+                p++;
+            } else {
+                d++;
+            }
+        }
     }
-    
 }
